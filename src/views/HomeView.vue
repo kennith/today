cc1f2e0c8ea21f35f7df2de77514703a589eeed1
Kennith Leung <kennith.leung@gmail.com>
2024-10-03 21:08:38 -0700

    add thumbnail of the current in progress project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup>
 import ThisWeek from '../components/ThisWeek.vue'
+import ProjectCard from '@/views/ProjectCard.vue';
+
+const projectObjects = [
+  { title: 'Habit Tracker', url: 'https://habit-tracker.kennith.dev', githubUrl: 'https://github.com/kennith/habit-tracker/' },
+  { title: 'Today', url: 'https://today.kennith.dev', githubUrl: 'https://github.com/kennith/today/' },
+];
+
+const columnCount = `row-cols-${projectObjects.length}`;
 </script>
 
 <template>
   <main>
     <ThisWeek></ThisWeek>
+    <div class="container my-3">
+      <div class="row" :class="columnCount">
+        <div v-for="(project, index) in projectObjects" v-bind:key="index">
+          <ProjectCard :project="project" class="col border  p-5"></ProjectCard>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
---
+++ src/views/ProjectCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+    project: Object
+}
+);
+</script>
+
+<template>
+    <div>
+        <div class="text-center">
+            <h2 class="mb-3">
+                <a :href="props.project.url" target="_blank" class="text-black link-underline link-underline-opacity-0">
+                    {{ props.project.title }}
+                </a>
+            </h2>
+
+            <a :href="props.project.githubUrl" target="_blank">
+                <i class=" bi bi-github fs-3 text-black"></i>
+            </a>
+        </div>
+    </div>
+</template>
