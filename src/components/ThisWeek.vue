ecfc749d490cad3bdaf5b072ae7f85a2bb6e95f9
Kennith Leung <kennith.leung@gmail.com>
2025-01-22 18:45:27 -0800

    Issues/10 (#12)

--- src/components/ThisWeek.vue
+++ src/components/ThisWeek.vue
@@ -1,21 +1,40 @@
 <template>
     <div class="text-center py-5" :class="backgroundColor()">
         <div class="fs-1 fw-bolder">
-            {{ weekLabel(weekOfYear).toUpperCase() }}
+            {{ weekLabelBanner }}
         </div>
         <div class="fw-thin">
             Week {{ weekOfYear }}
         </div>
 
-        <div>Next Week: {{ weekLabel(nextWeekOfYear) }}</div>
+        <div>Next Week: {{ nextWeekLabel }}</div>
     </div>
 </template>
 
 <script setup>
 import moment from 'moment';
+import { onMounted, ref } from 'vue';
 
-const weekOfYear = moment().week();
-const nextWeekOfYear = weekOfYear + 1;
+let weekOfYear = moment().week();
+let nextWeekOfYear = ref(weekOfYear.value + 1);
+let nextWeekLabel = ref(weekLabel(nextWeekOfYear));
+
+let changeWhenReload = ref('first');
+let weekLabelBanner = ref(weekLabel(weekOfYear));
+
+onMounted(() => {
+    window.addEventListener('focus', () => {
+        refreshData();
+    });
+})
+
+function refreshData() {
+    weekOfYear = moment().week();
+    changeWhenReload.value = (new Date().toString());
+    weekLabelBanner.value = weekLabel(weekOfYear);
+    nextWeekOfYear.value = weekOfYear + 1;
+    nextWeekLabel.value = weekLabel(nextWeekOfYear.value);
+}
 
 function isDevelopmentweek(weekOfYear) {
     return weekOfYear % 3 !== 0;
