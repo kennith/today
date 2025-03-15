6e4f685341f7a08183f30909a319b0db6af9662e
Kennith Leung <kennith.leung@gmail.com>
2025-03-14 17:25:08 -0700

    style: formatting (#16)
    * add animation when focus
    
    * remove unused code
    
    * fix
    
    * style: formatting

--- src/components/ThisWeek.vue
+++ src/components/ThisWeek.vue
@@ -1,11 +1,9 @@
 <template>
-    <div class="text-center py-5" :class="backgroundColor()">
+    <div class="text-center py-5" :class="thisWeekClass" id="this-week">
         <div class="fs-1 fw-bolder">
             {{ weekLabelBanner }}
         </div>
-        <div class="fw-thin">
-            Week {{ weekOfYear }}
-        </div>
+        <div class="fw-thin">Week {{ weekOfYear }}</div>
 
         <div>Next Week: {{ nextWeekLabel }}</div>
     </div>
@@ -13,7 +11,7 @@
 
 <script setup>
 import moment from 'moment';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 
 let weekOfYear = moment().week();
 let nextWeekOfYear = ref(weekOfYear.value + 1);
@@ -26,14 +24,21 @@ onMounted(() => {
     window.addEventListener('focus', () => {
         refreshData();
     });
-})
+});
+
+const shouldHide = ref(true);
 
 function refreshData() {
+    shouldHide.value = false;
     weekOfYear = moment().week();
-    changeWhenReload.value = (new Date().toString());
+    changeWhenReload.value = new Date().toString();
     weekLabelBanner.value = weekLabel(weekOfYear);
     nextWeekOfYear.value = weekOfYear + 1;
     nextWeekLabel.value = weekLabel(nextWeekOfYear.value);
+
+    setTimeout(() => {
+        shouldHide.value = true;
+    }, 300);
 }
 
 function isDevelopmentweek(weekOfYear) {
@@ -48,11 +53,8 @@ function weekLabel(week) {
     return 'Planning';
 }
 
-function backgroundColor() {
-    if (isDevelopmentweek(weekOfYear)) {
-        return 'bg-success-subtle';
-    }
-
-    return 'bg-primary-subtle';
-}
+let thisWeekClass = computed(() => ({
+    'bg-success-subtle': isDevelopmentweek(weekOfYear) && shouldHide.value,
+    'bg-primary-subtle': !isDevelopmentweek(weekOfYear) && shouldHide.value
+}));
 </script>
